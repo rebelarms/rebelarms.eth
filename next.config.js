02658bf56f1a5b0e715f14e92890b1fb46c6176a
Rebel Arms <RebelArms@protonmail.com>
2022-03-19 20:04:24 -0400

    Update next

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  assetPrefix: './',
-  TrailingSlash: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // assetPrefix: './',
+  // TrailingSlash: true,
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
 }
